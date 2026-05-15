4251e028fc0bd8326969109751869b03412a7be9
ahmadid15 <idrisahmadd15@gmail.com>
2026-05-15 11:46:13 +0100

    Exercise 10

---
+++ lpthw/ex10_escape.py
@@ -0,0 +1,16 @@
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list: 
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print("What Happened to you? {}".format(tabby_cat))
+print(f"What are your doing currently? {persian_cat}")
+print(backslash_cat)
+print(fat_cat)
+
