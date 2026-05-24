7702fa4ebe8c43b8ee4fc05f4a95fea4f0d86758
ahmadid15 <idrisahmadd15@gmail.com>
2026-05-24 14:14:15 +0100

    more exercises

---
+++ BFS/bfs_week_1_project.py
@@ -0,0 +1,8 @@
+# This is a Personal Info Card CLI
+prompt = '>>>'
+
+name = input("Kindly enter your name: ")
+fav_colur = input("Kindly enter your favourite colour here: ")
+age = input("Kindly enter your age: ")
+
+print(f"Welcome to our platform {name}, you are {age}, and your favourite colour is {fav_colur}")
